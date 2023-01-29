0de2eaf5bd7026364adc532fdb569af5d2f48b45
MPKreuzberg <fabrisigor01@gmail.com>
2023-01-29 10:29:13 +0100

    login and signup windows finished

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,22 +1,84 @@
- import React from 'react'
- import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
- const Auth = () => {
-   return (
-     <div>
-      <form>
-        <Box display="flex" flexDirection="column" alignItems="center" justifyContent={'center'}_>
-          <Typography>Login</Typography>
-          <TextField />
-          <TextField />
-          <TextField />
-          <Button>Submit</Button>
-          <Button>SignUp</Button>
-
+const Auth = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [isSignup, setIsSignup] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+  return (
+    <div>
+      <form onSubmit={handlesubmit}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent={"center"}
+          boxShadow="10px 10px 20px #ccc"
+          padding={3}
+          margin="auto"
+          marginTop={8}
+          borderRadius={5}
+          maxWidth="20rem"
+          _
+        >
+          <Typography variant="h2" pading={3} textAlign="center">
+            {isSignup ? "Signup" : "Login"}
+          </Typography>
+          {isSignup && (
+            <TextField
+              name="name"
+              onChange={handleChange}
+              value={inputs.name}
+              placeholder="Name"
+              margin="dense"
+            />
+          )}
+          <TextField
+            name="email"
+            onChange={handleChange}
+            value={inputs.email}
+            type={"email"}
+            placeholder="Email"
+            margin="dense"
+          />
+          <TextField
+            name="password"
+            onChange={handleChange}
+            value={inputs.password}
+            type={"password"}
+            placeholder="Password"
+            margin="dense"
+          />
+          <Button type="submit"
+            variant="contained"
+            sx={{ borderRadius: 3, marginTop: 3 }}
+            color="warning"
+          >
+            Submit
+          </Button>
+          <Button
+            onClick={() => setIsSignup(!isSignup)}
+            sx={{ borderRadius: 3, marginTop: 3 }}
+          >
+            {isSignup ? "Login" : "Signup"}
+          </Button>
         </Box>
       </form>
-     </div>
-   )
- }
- 
- export default Auth;
+    </div>
+  );
+};
+
+export default Auth;
